843016430fe404484bcde645f3481e0955b781d6
traudelx <julian.balletshofer@gmx.net>
2020-09-27 10:10:53 +0200

    first commit

---
+++ faceregapi/server.js
@@ -0,0 +1,40 @@
+const express = require('express');
+const bcrypt = require('bcrypt-nodejs');
+const cors = require('cors');
+const knex = require('knex');
+
+
+const register = require('./controllers/register.js');
+const signin = require('./controllers/signin.js');
+const profile = require('./controllers/profile');
+const image = require('./controllers/image')
+
+const db = knex({
+  client: 'pg',
+  connection: {
+    host : '127.0.0.1', // home local host
+    user : 'julianballetshofer',
+    password : '',
+    database : 'smart-brain'
+  }
+});
+
+const app = express();
+app.use(express.json());
+
+
+app.use(cors());
+
+
+app.post('/signin', (req,res)=> {signin.handleSignin(req,res, db, bcrypt)})
+
+app.post('/register', (req, res) => { register.handleRegister(req, res, db, bcrypt)}) // wir uebergeben die dependencies db und bcrypt der function so muss man es in dem file nicht extra definieren
+// andere moeglichkeit es zu definieren
+app.get('/profile/:id', profile.handleProfileGet(db))
+
+app.put('/image', image.handleImage(db))
+app.post('/imageurl', (req,res) => {image.handleApiCall(req,res)})
+
+app.listen(3000, ()=>{
+	console.log('app is running on port 3000')
+})
